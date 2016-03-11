2bbace2444866600689183a8ab48854bfe9c2d36
Jonathan50 <Jonathan50@users.noreply.github.com>
2016-03-12 11:23:51 +1300

    rename

--- sprites.py
+++ sprites.py
@@ -1,19 +1,19 @@
 # Copyright (C) 2016 Jeandre Kruger
 #
-# This file is part of screchifier.
+# This file is part of desnapifier.
 #
-# screchifier is free software: you can redistribute it and/or modify
+# desnapifier is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# screchifier is distributed in the hope that it will be useful,
+# desnapifier is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with screchifier.  If not, see <http://www.gnu.org/licenses/>.
+# along with desnapifier.  If not, see <http://www.gnu.org/licenses/>.
 
 import kurt
 import scripts
