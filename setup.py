b01f2acfc5ea807f699eb2b04ade5acf6fee969d
Jonathan50 <Jonathan50@users.noreply.github.com>
2016-03-12 13:55:05 +1300

    update setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version = "0.2.0",
     description = "Convert Snap! projects to Scratch",
     author = "Jeandre Kruger",
+    author_email = "Unknown",
     url = "https://github.com/Jonathan50/desnapifier",
     packages = [ "desnapifier" ]
 )
