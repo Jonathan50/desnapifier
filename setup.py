d720fedfab2b188289386d59f276a82903cfc6a8
Jonathan50 <Jonathan50@users.noreply.github.com>
2016-03-12 19:27:56 +1300

    bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ from distutils.core import setup
 
 setup(
     name = "desnapifier",
-    version = "0.6.0",
+    version = "0.6.1",
     description = "Convert Snap! projects to Scratch",
     author = "Jeandre Kruger",
     author_email = "Unknown",
